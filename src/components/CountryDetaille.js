52fa5de191fbb1668f8bb01ad1f868a5dc90966a
YassineSajjade <yassinesajjade@gmail.com>
2021-02-17 12:50:26 +0100

    fix td width

--- src/components/CountryDetaille.js
+++ src/components/CountryDetaille.js
@@ -50,23 +50,23 @@ const CountryDetaille = (props) => {
                                 <table>
                                     <tbody>
                                         <tr>
-                                            <td><span>Cases</span> </td>
+                                            <td className="tdWidth"><span>Cases</span> </td>
                                             <td> {props.location.cnt.cases} </td>
                                         </tr>
                                         <tr>
-                                            <td><span>Today Cases </span></td>
+                                            <td className="tdWidth"><span>Today Cases </span></td>
                                             <td> {props.location.cnt.todayCases} </td>
                                         </tr>
                                         <tr>
-                                            <td><span>Deaths </span></td>
+                                            <td className="tdWidth"><span>Deaths </span></td>
                                             <td> {props.location.cnt.deaths} </td>
                                         </tr>
                                         <tr>
-                                            <td><span>Today Deaths </span></td>
+                                            <td className="tdWidth"><span>Today Deaths </span></td>
                                             <td> {props.location.cnt.todayDeaths} </td>
                                         </tr>
                                         <tr>
-                                            <td><span>Recovered </span></td>
+                                            <td className="tdWidth"><span>Recovered </span></td>
                                             <td> {props.location.cnt.recovered} </td>
                                         </tr>
                                     </tbody>
@@ -77,23 +77,23 @@ const CountryDetaille = (props) => {
                             <table>
                                     <tbody>
                                         <tr>
-                                            <td><span>Today Recovered </span></td>
+                                            <td className="tdWidth"><span>Today Recovered </span></td>
                                             <td> {props.location.cnt.todayRecovered} </td>
                                         </tr>
                                         <tr>
-                                            <td><span>Active </span></td>
+                                            <td className="tdWidth"><span>Active </span></td>
                                             <td> {props.location.cnt.active} </td>
                                         </tr>
                                         <tr>
-                                            <td><span>Critical </span></td>
+                                            <td className="tdWidth"><span>Critical </span></td>
                                             <td> {props.location.cnt.critical} </td>
                                         </tr>
                                         <tr>
-                                            <td><span>Cases Per One Million </span></td>
+                                            <td className="tdWidth"><span>Cases Per One Million </span></td>
                                             <td> {props.location.cnt.casesPerOneMillion} </td>
                                         </tr>
                                         <tr>
-                                            <td><span>Deaths Per One Million </span></td>
+                                            <td className="tdWidth"><span>Deaths Per One Million </span></td>
                                             <td> {props.location.cnt.deathsPerOneMillion} </td>
                                         </tr>
                                         
